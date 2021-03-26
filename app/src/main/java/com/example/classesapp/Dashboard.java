ceb2a00f90935d4627889fbaf737b66cb7dfe21c
Ritika94168 <ritikagaba2501@gmail.com>
2021-03-26 12:50:45 +0530

    Initialnew

--- app/src/main/java/com/example/classesapp/Dashboard.java
+++ app/src/main/java/com/example/classesapp/Dashboard.java
@@ -15,6 +15,7 @@ import android.os.Bundle;
 import android.util.EventLogTags;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.GridLayout;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -24,16 +25,16 @@ import example.javatpoint.com.introonetimefirsttime.R;
 
 public class Dashboard extends AppCompatActivity {
     List<MyList>myLists;
-    RecyclerView rv;
+    GridLayout rv;
     MyAdapter adapter;
     ProgressDialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
-        rv=(RecyclerView)findViewById(R.id.rec);
-        rv.setHasFixedSize(true);
-        rv.setLayoutManager(new GridLayoutManager(this,3));
+        setContentView(R.layout.activity_dashboard_new);
+        rv=(GridLayout) findViewById(R.id.rec);
+//        rv.setHasFixedSize(true);
+//        rv.setLayoutManager(new GridLayoutManager(this,3));
         myLists=new ArrayList<>();
         ConnectivityManager ConnectionManager=(ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo=ConnectionManager.getActiveNetworkInfo();
@@ -51,36 +52,36 @@ public class Dashboard extends AppCompatActivity {
                 .setMessage(getResources().getString(R.string.internet_error))
                 .setPositiveButton("OK", null).show();
         }
-        getdata();
+//        getdata();
     }
 
-    private void getdata() {
-        myLists.add(new MyList(R.drawable.student));
-        myLists.add(new MyList(R.drawable.ic_discount));
-        myLists.add(new MyList(R.drawable.ic_discount));
-        myLists.add(new MyList(R.drawable.ic_discount));
-        myLists.add(new MyList(R.drawable.ic_discount));
-        myLists.add(new MyList(R.drawable.ic_discount));
-        myLists.add(new MyList(R.drawable.ic_discount));
-        myLists.add(new MyList(R.drawable.ic_discount));
-
-        adapter=new MyAdapter(myLists,this);
-        rv.setAdapter(adapter);
-      rv.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
-          @Override
-          public boolean onInterceptTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
-              return false;
-          }
-
-          @Override
-          public void onTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
-
-          }
-
-          @Override
-          public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
-
-          }
-      });
-    }
+//    private void getdata() {
+//        myLists.add(new MyList(R.drawable.student));
+//        myLists.add(new MyList(R.drawable.ic_discount));
+//        myLists.add(new MyList(R.drawable.ic_discount));
+//        myLists.add(new MyList(R.drawable.ic_discount));
+//        myLists.add(new MyList(R.drawable.ic_discount));
+//        myLists.add(new MyList(R.drawable.ic_discount));
+//        myLists.add(new MyList(R.drawable.ic_discount));
+//        myLists.add(new MyList(R.drawable.ic_discount));
+//
+//        adapter=new MyAdapter(myLists,this);
+//        rv.setAdapter(adapter);
+//      rv.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+//          @Override
+//          public boolean onInterceptTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
+//              return false;
+//          }
+//
+//          @Override
+//          public void onTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
+//
+//          }
+//
+//          @Override
+//          public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+//
+//          }
+//      });
+//    }
 }
